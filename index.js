ba5ba37ab44f97ef9c6c7aee9cbeeaba0c25303f
Usman Muhammad Hassan <hassanusman5667@gmail.com>
2022-09-18 18:59:51 +0100

    created a  basic server

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// we bring in the logger class we exported
-const Logger = require('./logger')
+// we are going to create a server
 
-// create a new logger object called myLogger
-const myLogger = new Logger();
+// we firstly bring in our modules
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-// init event on the new class object
-myLogger.on('message', (data) => console.log('called event', data))
+// now we are going to store our server in a variable called server
+const server = http.createServer((req,res) =>{
+    console.log(req.url)
+    if (req.url === '/'){
+        res.end('<h1>Home</h1>')
+    }
+}) 
 
-// call the log method and pass in the message
-myLogger.log('hello world')
+// our port number could change after deplyment so it would give us an error  
+// so we are going to check the env before adding the port
+const PORT = process.env.PORT || 5000
+// we will be adding the .listen to the variable name
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
